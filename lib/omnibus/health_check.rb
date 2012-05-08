aa660d9b5874fa57125afb468b327d271965dfb9
Stephen Nelson-Smith <sns@opscode.com>
2012-05-08 14:12:21 +0100

    Improve information on verbose run, and provide a summary in case of failed health check.

--- lib/omnibus/health_check.rb
+++ lib/omnibus/health_check.rb
@@ -50,7 +50,9 @@ module Omnibus
                               /libsec\.so/,
                               /libsocket\.so/,
                               /libssl.so/,
-                              /libuutil\.so/
+			      /libthread.so/,
+                              /libuutil\.so/,
+			      /libz.so/
                              ]
     
     WHITELIST_FILES = [
@@ -86,9 +88,12 @@ module Omnibus
         case line
         when /^(.+):$/
           current_library = $1
+	  log "*** Analysing dependencies for #{current_library}" if ARGV[0] == "verbose"
         when /^\s+(.+) \=\>\s+(.+)( \(.+\))?$/
           name = $1
           linked = $2
+	  log "  --> Dependency: #{name}" if ARGV[0] == "verbose"
+	  log "  --> Provided by: #{linked}" if ARGV[0] == "verbose"
           safe = nil
           WHITELIST_LIBS.each do |reg| 
             safe ||= true if reg.match(name)
@@ -98,15 +103,17 @@ module Omnibus
           end
 
           if !safe && linked !~ Regexp.new(install_dir)
+	    log "    -> FAILED: #{current_library} has unsafe dependencies" if ARGV[0] == "verbose"
             bad_libs[current_library] ||= {}
             bad_libs[current_library][name] ||= {} 
             if bad_libs[current_library][name].has_key?(linked)
               bad_libs[current_library][name][linked] += 1 
+              
             else
               bad_libs[current_library][name][linked] = 1 
             end
           else
-            log "Passed: #{current_library} #{name} #{linked}" if ARGV[0] == 'verbose'
+            log "    -> PASSED: #{name} is either whitelisted or safely provided." if ARGV[0] == "verbose"
           end
         when /^\s+(.+) \(.+\)$/
           next
@@ -120,21 +127,54 @@ module Omnibus
           next
         when /^\s+not a dynamic executable$/ # ignore non-executable files
         else
-          log "line did not match for #{current_library}\n#{line}"
+          log "*** Line did not match for #{current_library}\n#{line}"
         end
       end
 
       File.delete('ldd.out')
+      
 
+      unresolved = []
+      unreliable = []
+      detail = []
       if bad_libs.keys.length > 0
         bad_libs.each do |name, lib_hash|
           lib_hash.each do |lib, linked_libs|
-            linked_libs.each do |linked, count|
-              log "#{name}: #{lib} #{linked} #{count}"
+           linked_libs.each do |linked, count|
+              if linked =~ /not found/
+                unresolved << lib unless unresolved.include? lib
+              else
+                unreliable << linked unless unreliable.include? linked
+              end
+                detail << "#{name}|#{lib}|#{linked}|#{count}"
             end
           end
         end
-        raise "Health Check Failed"
+        log "*** Health Check Failed, Summary follows:"
+        bad_omnibus_libs, bad_omnibus_bins = bad_libs.keys.partition { |k| k.include? "embedded/lib" }
+        log "*** The following Omnibus-built libraries have unsafe or unmet dependencies:"
+        bad_omnibus_libs.each { |lib| log "    --> #{lib}" }
+        log "*** The following Omnibus-built binaries have unsafe or unmet dependencies:"
+        bad_omnibus_bins.each { |bin| log "    --> #{bin}" }
+        if unresolved.length > 0
+          log "*** The following requirements could not be resolved:"
+          unresolved.each { |lib| log "    --> #{lib}"}
+        end
+        if unreliable.length > 0
+          log "*** The following libraries cannot be guaranteed to be on target systems:"
+          unreliable.each { |lib| log "    --> #{lib}"}
+        end
+        log "*** The precise failures were:"
+        detail.each do |line|
+          item, dependency, location, count = line.split('|')
+          reason = location =~ /not found/ ? "Unresolved dependency" : "Unsafe dependency"
+          log "    --> #{item}"
+          log "    DEPENDS ON: #{dependency}"
+          log "      COUNT: #{count}"
+          log "      PROVIDED BY: #{location}"
+          log "      FAILED BECAUSE: #{reason}"
+        end
+	raise "Health Check Failed"
       end
     end
 
